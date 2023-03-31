b4a5fa6044f4c8f61d9946f52e418d541f6d7a11
Estudiante USC <estudiante@GTACAD.USC>
2023-03-31 16:25:37 -0500

    taller7

---
+++ taller7/excepcion.java
@@ -0,0 +1,16 @@
+
+public class excepcion {
+
+	import java.util.Scanner;
+
+	public class CuadradoNumero {
+	    public static void main(String[] ar) {
+	        Scanner teclado = new Scanner(System.in);
+	        int num;
+	        System.out.print("Ingrese un valor entero:");
+	        num = teclado.nextInt();
+	        int cuadrado = num * num;
+	        System.out.print("El cuadrado de " + num + " es " + cuadrado);
+	    }
+	}
+}
---
+++ taller7/src/taller7.java
@@ -0,0 +1,16 @@
+
+public class taller7 {
+	
+	public static class sintaxis
+	public static void main (String[]args) {
+	
+	String str = “Hello World”
+
+
+	for(int i = 0; i < 4; i++);
+	{
+	System.out.println(str); str+= ”!”;
+	}
+
+
+}
